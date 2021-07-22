f1215215f4fcd5e579181886e39100f917755b0a
Neha Adnekar <43112829+Nehaadnekar@users.noreply.github.com>
2021-07-22 23:48:22 +0530

    Create SearchInsertPosition.java

---
+++ BinarySearch/SearchInsertPosition.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0;
+        int n = nums.length;
+        int r = n-1;
+        int mid =0;
+        while(r>=l){
+            mid = l+(r-l)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[mid]>target){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+            
+        }
+        return l;
+    }
+}
