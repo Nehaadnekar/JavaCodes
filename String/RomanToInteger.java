5ad2685279881d0dab48817177322ba4cc35c8c1
Neha Adnekar <43112829+Nehaadnekar@users.noreply.github.com>
2021-07-26 23:51:04 +0530

    Update RomanToInteger.java

--- String/RomanToInteger.java
+++ String/RomanToInteger.java
@@ -10,6 +10,7 @@ class Solution {
         map.put('D',500);
         map.put('M',1000);
         int res = 0;
+        
         for(int i=0;i<s.length()-1;i++){
             if(map.get(s.charAt(i))>=map.get(s.charAt(i+1))){
                 res+=map.get(s.charAt(i));
@@ -18,6 +19,7 @@ class Solution {
                 res-=map.get(s.charAt(i));
             }
         }
+        
         res+=map.get(s.charAt(s.length()-1));
         return res;
     }
